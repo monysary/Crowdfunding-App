55b763c9b09a049628ef0adaf992cd14adf89452
monysary <sary.mony@gmail.com>
2022-12-21 13:03:25 -0800

    pushing code to branch

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.use(session(sess));
 // setting up handlebars engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
