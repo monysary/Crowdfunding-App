438f2e68dfe1ccf46f87344a4fdd528fda90ad26
Brian Wang <wangbrian26@gmail.com>
2022-12-21 11:35:37 -0800

    updated profile.js

--- controllers/profile.js
+++ controllers/profile.js
@@ -4,23 +4,27 @@ const { Project } = require('../models');
 
 profile.get('/', withAuth, async (req, res) => {
   try {
-    const userProjects = await Project.findAll( where: {
-      user_id: req.session.user_id,
-      include: [
-        {
-          model: Project,
-          attributes: [
-            'id',
-            'name',
-            'description',
-            `date_created`,
-            `needed_funding`,
-          ],
-        },
-      ],
+    const userProjects = await Project.findAll({
+      where: {
+        user_id: req.session.user_id,
+        include: [
+          {
+            model: Project,
+            attributes: [
+              'id',
+              'name',
+              'description',
+              `date_created`,
+              `needed_funding`,
+            ],
+          },
+        ],
+      },
     });
 
-    const projects = userProjects.map((project) => project.get({ plain: true }));
+    const projects = userProjects.map((project) =>
+      project.get({ plain: true })
+    );
 
     res.render('profile', projects);
   } catch (err) {
